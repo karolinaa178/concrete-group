92b1314211388191862d4c7e62b46b733a49f7b6
Karolina Okła <karolina@MBP-Karolina.Dlink>
2022-01-09 13:24:38 +0100

    Contact form in progress

--- concreteGroup/src/app/app.module.ts
+++ concreteGroup/src/app/app.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule, Routes } from '@angular/router';
-import { ReactiveFormsModule } from '@angular/forms';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import {MatButtonModule} from '@angular/material/button';
+import {MatRippleModule} from '@angular/material/core';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
@@ -10,6 +15,7 @@ import { OfferComponent } from './offer/offer.component';
 import { FooterComponent } from './footer/footer.component';
 import { ProjectsComponent } from './projects/projects.component';
 import { ContactComponent } from './contact/contact.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 const appRoutes: Routes = [
   { path: '', component: HomeComponent },
@@ -18,6 +24,12 @@ const appRoutes: Routes = [
   { path: 'kontakt', component: ContactComponent }
 ];
 
+const MATERIAL_MODULES = [
+  MatFormFieldModule,
+  MatInputModule,
+  MatButtonModule,
+  MatRippleModule,
+];
 @NgModule({
   declarations: [
     AppComponent,
@@ -30,8 +42,11 @@ const appRoutes: Routes = [
   ],
   imports: [
     BrowserModule,
+    MATERIAL_MODULES,
+    FormsModule,
     ReactiveFormsModule,
-    RouterModule.forRoot(appRoutes)
+    RouterModule.forRoot(appRoutes),
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- concreteGroup/src/app/contact/contact.component.ts
+++ concreteGroup/src/app/contact/contact.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ColorTypeEnum } from 'src/enums/color-type.enum';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
@@ -10,9 +11,25 @@ export class ContactComponent implements OnInit {
 
   ColorTypeEnum = ColorTypeEnum;
 
+  contactForm: FormGroup;
+
   constructor() { }
 
   ngOnInit(): void {
+    this.contactForm = new FormGroup({
+      'name': new FormControl('', Validators.required),
+      'lastname': new FormControl('', Validators.required),
+      'email': new FormControl('', [Validators.required, Validators.email]),
+      'phone': new FormControl('', Validators.required),
+      'message': new FormControl('', Validators.required)
+    });
+  }
+
+
+  // TODO: dodać działający przycisk do formularza aby wysyłało poprawnie
+  onSubmit(): void {
+    console.log(this.contactForm);
+
   }
 
 }
